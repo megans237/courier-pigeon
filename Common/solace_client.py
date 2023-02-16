7bcb097ad1313c7b9d4853218a0975c6ce9e0de3
Anthony Luo <anthonyluo100@gmail.com>
2023-02-15 22:14:36 -0500

    solace client class base init up

--- Common/solace_client.py
+++ Common/solace_client.py
@@ -2,9 +2,11 @@ import dotenv
 import os
 import certifi
 import paho.mqtt.client as mqtt
-class SolaceClient():
-    def __init__(self):
+class SolaceClient:
+    def __init__(self) -> mqtt.Client:
         """Initializes default client information
+
+        :return: connected mqtt.Client class
         """
         dotenv.load_dotenv()
         self.USR = os.getenv("SOLACE_USERNAME")
@@ -13,3 +15,6 @@ class SolaceClient():
         self.PRT = os.getenv("SOLACE_PORT")
         self.DBG = int(os.getenv("DEBUG"))
 
+        # connect to something somehow
+
+        # return the client
