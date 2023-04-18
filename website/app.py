a812b02f7cb016d1713c7d9e9458457948db71b4
Megan <74023274+megans237@users.noreply.github.com>
2023-04-17 21:33:32 -0400

    site with key removed, origin error maps embed

--- website/app.py
+++ website/app.py
@@ -9,17 +9,29 @@ from flask_moment import Moment
 from sys import stderr
 moment = Moment(app)
 
+print("If Error: Could not locate a Flask application, please enter <cd website>")
+
+# import Common.solace_client
+# import Common.common_datatypes as pigeon_dtype
+# import Supervisor.router as router
+
+# import dotenv
+# import os
+# import certifi
+# import paho.mqtt.client as mqtt
+
+# api key 
 
 # solace comms setup
+
 # for now a list of coords passed in
 inputs = ("45.422195,-75.681503;45.423646,-75.682736;45.424803,-75.684954;45.426898,-75.686144;45.421772,-75.696036;45.419973,-75.695602;45.418594,-75.692945;45.409996,-75.699655;45.407108,-75.690199;45.402732,-75.693199;45.403115,-75.696691;45.422195,-75.681503")
 
 @app.route("/")
 def home():
-    map_link = "https://www.google.com/maps/embed/v1/view?key=AIzaSyD4iE2xVSpkLLOXoyqT-RuPwURN3ddScAI&amp;center=-33.8569,151.2152&amp;zoom=18&amp;maptype=satellite"
-    #map_generate(inputs)
-    print(map_link, file=stderr)
-    return render_template("home.html", map_link=map_link)
+    map = map_generate(inputs)
+    print(map, file=stderr)
+    return render_template("home.html", map=map)
 
 @app.route("/scan/")
 def about():
@@ -43,12 +55,22 @@ def test():
 def map_generate(inputs):
 
     coords = inputs.split(";")
-    url = "https://www.google.com/maps/dir/?api=1&origin=" + coords[0] + "&destination=" + coords[-1] + "travelmode=driving&waypoints="
-    for x in range(1, (len(coords))):
-        url = url + coords[x] + "%7C"
+    print(len(coords))
+    print(coords[0])
+    url = "https://www.google.com/maps/embed/v1/directions?key=___&origin=" + str(coords[0]) + "&waypoints="
+    for x in range(1, (len(coords))-2):
+        url = url + coords[x] + "|" 
         x += 1
+        print(url)
+        print(x)
+        if x == len(coords)-2:
+            url = url[:-1]
    
+    url = url + "&destination=" + coords[len(coords)-2] + "&zoom=14"
     return url
+
+print(map_generate(inputs))
+
 
 
 
